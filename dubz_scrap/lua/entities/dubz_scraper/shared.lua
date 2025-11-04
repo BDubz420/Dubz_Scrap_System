2f87dcc61770f83db2ada720f1274af62497aa73
BDubz420 <129692319+BDubz420@users.noreply.github.com>
2025-11-03 23:54:22 -0500

    Delete dubz_scrap/lua/entities/dubz_scraper directory

--- dubz_scrap/lua/entities/dubz_scraper/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "base_gmodentity"
-ENT.Spawnable = true
-ENT.AdminSpawnable = true
-
-ENT.PrintName = "Scraper"
-ENT.Category = "Dubz Scrap System"
-ENT.Author = "BDubz420 - Lowkey Network"
